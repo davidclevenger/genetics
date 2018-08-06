484a44300d8c5a21709d79cde264f73770217f3a
David Clevenger <dclevenger00@gmail.com>
2018-08-05 22:09:20 -0500

    Accidentally checked in object files. Finished python script to run tests after build.

--- tests/run_all.py
+++ tests/run_all.py
@@ -1,6 +1,20 @@
 import os
+import subprocess
+import sys
+
+def run_all():
+	"""
+	Run all tests. Find all files with '.o' extension.
+	Run the corresponding executable.
+	"""
+	files = os.listdir()
+	
+	for f in files:
+		if f.endswith('.o'): # assume executable exists if object file exists
+			exe_str = "./{}".format(f[:-2])
+			proc = subprocess.Popen(exe_str, shell=False)
+			proc.wait()
 
 if __name__ == '__main__':
-	# get all files with '.c' extension
-	# run the corresponding executable
+	sys.exit(run_all())
 
